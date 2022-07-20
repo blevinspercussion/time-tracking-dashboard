09182b0f16a18511587c5b4bee5c355a3498a8ad
Adam Blevins <blevinspercussion@gmail.com>
2022-07-20 12:52:26 -0500

    Imported JSON data into App component and added DetailCard map to the return

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import UserCard from './components/UserCard';
 import DetailCard from './components/DetailCard';
 
+import cardInfo from './data.json';
+
 function App() {
+
+  const info = cardInfo;
+
   return (
     <div className="App">
       <div id='main-content'>
         <UserCard />
+        {info.map((item) => {
+          <DetailCard 
+            key={item.title}
+            item={item}
+            title={item.title}
+            current={item.timeframes.daily.current}
+            previous={item.timeframes.daily.previous}
+            />
+        })}
       </div>
     </div>
   );
